33baa660ff701be5cf03a45f8c5a3143150925b8
Filip Madzia <filipmadzia33@gmail.com>
2024-09-25 17:37:28 +0200

    Reading file

--- exploring-files/exploring-files.cpp
+++ exploring-files/exploring-files.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+void writing_file()
+{
+    ofstream fileToWrite("test_file.txt");
+
+    fileToWrite << "Hello world" << endl;
+
+    fileToWrite.close();
+}
+
+void reading_file()
+{
+    ifstream readFile("test_file.txt");
+
+    if (!readFile.is_open()) {
+        cerr << "Error opening the file!" << endl;
+    }
+
+    string line;
+
+    while (getline(readFile, line))
+    {
+        cout << line << endl;
+    }
+
+    readFile.close();
+}
+
 int main()
 {
-    ofstream testFile("test_file.txt");
-
-    testFile << "Hello world" << endl;
-
-    testFile.close();
+    reading_file();
 }
